95e06a94b21442f3ddcfda8ec51986fdeb775c8a
Shayan Golafshani <shayangolafsh@gmail.com>
2021-02-23 16:58:21 -0700

    Add Javascript variables of different types

---
+++ variablePractice.js
@@ -0,0 +1,12 @@
+var numChildren = 13;
+
+var firstGraders = ['Harry', 'Becca', 'Tommy', 'Suzie', 'Adolph', 'Nancy', 'Terrence'
+ 'Jerry', 'John', 'Hagrid', 'Michelle', 'Barry', 'Garret']
+
+ var isPresent = true;
+
+ var schoolGreeting = "Good morning kids, welcome to Calvin S. Smith Elementary School!"
+
+ var recessLength = 35;
+
+ var recessGames = ['dodgeball', 'four square', 'tetherball', 'kickball', 'basketball', 'tag']
