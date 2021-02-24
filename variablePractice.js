8f0e82ac2dbdeba08dcf45021e2d05eca1b2eed1
Shayan Golafshani <shayangolafsh@gmail.com>
2021-02-23 17:01:36 -0700

    Reassign variable values

--- variablePractice.js
+++ variablePractice.js
@@ -1,12 +1,18 @@
 var numChildren = 13;
 
 var firstGraders = ['Harry', 'Becca', 'Tommy', 'Suzie', 'Adolph', 'Nancy', 'Terrence'
- 'Jerry', 'John', 'Hagrid', 'Michelle', 'Barry', 'Garret']
+ 'Jerry', 'John', 'Hagrid', 'Michelle', 'Barry', 'Garret'];
 
  var isPresent = true;
 
- var schoolGreeting = "Good morning kids, welcome to Calvin S. Smith Elementary School!"
+ var schoolGreeting = "Good morning kids, welcome to Calvin S. Smith Elementary School!";
 
  var recessLength = 35;
 
- var recessGames = ['dodgeball', 'four square', 'tetherball', 'kickball', 'basketball', 'tag']
+ var recessGames = ['dodgeball', 'four square', 'tetherball', 'kickball', 'basketball', 'tag'];
+
+ var recessLength = 50;
+
+ var recessGames = ['Red Rover'];
+
+ var numChildren = 18;
